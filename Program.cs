ef9a7d6bb881dc1deeccfa85ba0c36c83b12490e
AliakseiDziatlau <aliakseidziatlau@gmail.com>
2025-01-09 21:01:28 +0100

    feature/remove comments in Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,6 @@ using DocumentsService.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var enviroment = builder.Environment.EnvironmentName;
 ConfigurationSetup.AddCustomConfiguration(builder.Configuration, builder.Environment);
 
 builder.Services.AddCustomServices(builder.Configuration);
@@ -20,53 +19,3 @@ var app = builder.Build();
 SwaggerSetup.ConfigureSwagger(app);
 MiddlewareSetup.ConfigureMiddleware(app);
 app.Run();
-
-
-
-// builder.Configuration
-//     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-//     .AddJsonFile($"appsettings.{enviroment}.json", optional: true, reloadOnChange: true)
-//     .AddUserSecrets<Program>(optional: true)
-//     .AddEnvironmentVariables();
-//
-//
-// //DI
-// builder.Services.AddScoped<IDocumentService, DocumentService>();
-// builder.Services.AddScoped<IBlobStorageRepository>(provider =>
-// {
-//     var configuration = provider.GetRequiredService<IConfiguration>();
-//     var connectionString = configuration["AzureStorage:BlobConnectionString"];
-//     var containerName = configuration["AzureStorage:BlobContainerName"];
-//     return new BlobStorageRepository(connectionString, containerName);
-// });
-//
-// builder.Services.AddScoped<ITableStorageRepository>(provider =>
-// {
-//     var configuration = provider.GetRequiredService<IConfiguration>();
-//     var connectionString = configuration["AzureStorage:TableConnectionString"];
-//     var tableName = configuration["AzureStorage:TableName"];
-//     return new TableStorageRepository(connectionString, tableName);
-// });
-//
-// builder.Services.AddControllers();
-//
-// builder.Services.AddSwaggerGen();
-// builder.Services.AddHttpClient();
-// var app = builder.Build();
-//
-// if (app.Environment.IsDevelopment())
-// {
-//     app.UseDeveloperExceptionPage();
-//     app.UseSwagger();
-//     app.UseSwaggerUI(c =>
-//     {
-//         c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"); 
-//     });
-// }    
-//
-// app.UseHttpsRedirection();
-// app.UseRouting();
-// app.UseAuthentication();
-// app.UseAuthorization();
-// app.MapControllers();
-// app.Run();
